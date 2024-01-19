fed8464acf72229720412b9f429f0d5d3a6bdf3b
dropthedead <dropdeaddropdead@yandex.ru>
2024-01-19 14:36:01 +0300

    feat: 01-read-file task done

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const filePath = '01-read-file/text.txt';
+const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
+readStream.on('data', (chunk) => {
+  console.log(chunk);
+});
